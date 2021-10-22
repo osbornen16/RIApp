24cec0415dac86d181c6945158af6ed1e68901cb
Nate Osborne <osbornen16@gmail.com>
2021-10-22 10:05:23 -0400

    controller and service and models built

---
+++ RI.Models/CategoryCreate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RI.Models
+{
+    public class CategoryCreate
+    {
+        [Required]
+        public int CategoryId { get; set; }
+        [Required]
+        public string CategoryName { get; set; }
+    }
+}
---
+++ RI.Models/CategoryDetail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RI.Models
+{
+    public class CategoryDetail
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        [ForeignKey(nameof(Product))]
+        public int Id { get; set; }
+        [ForeignKey(nameof(Product))]
+        public int Description { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
