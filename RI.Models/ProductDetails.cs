807cfedf73ce0609793e26dedbfa44c1927d56d9
Krystle Taniguchi <ktanig87@gmail.com>
2021-10-22 07:48:43 -0600

    add Product Models

---
+++ RI.Models/ProductDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RI.Models
+{
+    public class ProductDetails
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        
+        public double Price { get; set; }
+        public string Description { get; set; }
+
+    }
+}
---
+++ RI.Service/ProductService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RI.Service
+{
+    public class ProductService
+    {
+    }
+}
