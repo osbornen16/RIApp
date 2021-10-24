dac8cbaedf44f4e06ec116c432a3c8839adf1764
Krystle Taniguchi <ktanig87@gmail.com>
2021-10-24 14:01:39 -0600

    started Product Service

--- RI.Service/ProductService.cs
+++ RI.Service/ProductService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RI.Data;
+using RI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,35 @@ namespace RI.Service
 {
     public class ProductService
     {
+        private readonly Guid _userId;
+
+        public ProductService(Guid userId)
+        { _userId = userId; }
+
+        public bool CreateProduct(ProductCreate model)
+        { 
+            var entity = 
+                new Product
+        }
+
+        public IEnumerable<ProductList> GetProducts()
+        {
+
+        }
+
+        public ProductDetails GetProductById(int id)
+        {
+
+        }
+
+        public bool UpdateProduct(ProductEdit model)
+        {
+
+        }
+
+        public bool DeleteProduct(int id)
+        {
+
+        }
     }
 }
--- RelativelyIrrelevantApp/Controllers/ProductController.cs
+++ RelativelyIrrelevantApp/Controllers/ProductController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using RI.Models;
+using RI.Service;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,6 +12,54 @@ namespace RelativelyIrrelevantApp.Controllers
 {
     public class ProductController : ApiController
     {
+        private ProductService CreateProductService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var productService = new ProductService(userId);
+            return productService;
+        }
 
+        public IHttpActionResult Get()
+        {
+            ProductService productService = CreateProductService();
+            var products = productService.GetProducts();
+            return Ok(products);
+        }
+
+        public IHttpActionResult Post(ProductCreate model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var service = CreateProductService();
+            if (!service.CreateProduct(model))
+                return InternalServerError();
+            return Ok();
+        }
+
+        public IHttpActionResult Get(int iD)
+        {
+            ProductService noteService = CreateProductService();
+            var note = noteService.GetProductById(iD);
+            return Ok(note);
+        }
+
+        public IHttpActionResult Put(ProductEdit product)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateProductService();
+            if (!service.UpdateProduct(product))
+                return InternalServerError();
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateProductService();
+            if (!service.DeleteProduct(id))
+                return InternalServerError();
+            return Ok();
+        }
     }
 }
