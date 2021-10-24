c37a527e5d85b0785471917070d7353591381c11
Nate Osborne <osbornen16@gmail.com>
2021-10-24 18:07:09 -0400

    updated errors

--- RI.Models/CategoryListItem.cs
+++ RI.Models/CategoryListItem.cs
@@ -11,13 +11,8 @@ namespace RI.Models
 {
     public class CategoryListItem
     {
-        [Key]
         public int CategoryId { get; set; }
         public string CategoryName { get; set; }
-        [ForeignKey(nameof(Product))]
-        public int Id { get; set; }
-        [ForeignKey(nameof(Product))]
-        public int Description { get; set; }
-        public virtual Product Product { get; set; }
+        public List<Product> ProductList { get; set; }
     }
 }
